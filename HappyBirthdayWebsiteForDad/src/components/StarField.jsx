4ccd45ae8c21d0d28535da4c3cda1efa691100c4
AustinCLieu <asianonrice@gmail.com>
2026-03-19 02:04:18 -0700

    Changed star colors to have white', 'red', 'lime', 'dodgerblue', 'yellow', 'mediumpurple', 'orange'

--- HappyBirthdayWebsiteForDad/src/components/StarField.jsx
+++ HappyBirthdayWebsiteForDad/src/components/StarField.jsx
@@ -10,20 +10,22 @@ export default function StarField() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
+    const colors = ['white', 'red', 'lime', 'dodgerblue', 'yellow', 'mediumpurple', 'orange']
+
     const stars = Array.from({ length: 150 }, () => ({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
       radius: Math.random() * 1.5 + 0.5,
       speed: Math.random() * 1.5 + 0.5,
+      color: colors[Math.floor(Math.random() * colors.length)],
     }))
 
     let animFrameId
 
     function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.fillStyle = 'white'
-
       for (const star of stars) {
+        ctx.fillStyle = star.color
         star.y += star.speed
         if (star.y > canvas.height) {
           star.y = 0
