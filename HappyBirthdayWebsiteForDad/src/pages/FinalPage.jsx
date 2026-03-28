ad8d98ad00cb1041ecd4a6d4439636908fe7e780
AustinCLieu <asianonrice@gmail.com>
2026-03-28 16:23:34 -0700

    Added happy birthday song at the end.

--- HappyBirthdayWebsiteForDad/src/pages/FinalPage.jsx
+++ HappyBirthdayWebsiteForDad/src/pages/FinalPage.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useRef } from 'react'
+import happyBirthday from '../assets/the_mountain-happy-birthday-508020.mp3'
+
 function FinalPage() {
+  const audioRef = useRef(null)
+
+  useEffect(() => {
+    audioRef.current.play()
+  }, [])
+
   return (
     <div>
+      <audio ref={audioRef} src={happyBirthday} />
       <h1>You passed! Happy Birthday Dad!</h1>
     </div>
   )
